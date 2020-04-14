f5945258e58a8efcfc5249ece4466987c3b89942
Laura Hasemeyer <lhasemeyer@twitter.com>
2020-04-14 14:28:21 -0600

    simplifying bowling into more functions

--- Code-Wars/bowling-CW.js
+++ Code-Wars/bowling-CW.js
@@ -4,81 +4,54 @@ function bowling(frames) {
   const additionalSpareCounter = {};
   frames.split(' ').forEach((frame, index) => {
     frame.split('').forEach((bowl, bowlIndex) => {
-      const isFrameTen = (bowl === 'X' || frame.includes('/')) ? true : false;
       const bowlInteger = parseInt(bowl);
       if(index !== 9) {
-        if(bowl === 'X') {
-          additionalStrikeCounter[index] = 0;
-          score += 10;
-        } else if(frame.includes('/')) {
-          additionalSpareCounter[index] = 0;
-          if(bowlIndex === 1) {
-            score += 10;
-          }
-        } else {
-          score += bowlInteger;
-        }
-        
+        // not last frame
+         if(bowl === 'X') {
+           additionalStrikeCounter[index] = 0;
+           score += 10;
+         } else if(frame.includes('/')) {
+           additionalSpareCounter[index] = 0;
+           if(bowlIndex === 1) {
+             score += 10;
+           }
+         } else {
+           score += bowlInteger;
+         }
         // if the last frame had a strike, add current bowl.  if x, add 10.  if frame has spare, only add 10 on second bowl
         if((additionalStrikeCounter[index-1] !== undefined)) {
-          if(isFrameTen && ((bowl === 'X') || (bowlIndex === 1))) {
-            additionalStrikeCounter[index-1] = additionalStrikeCounter[index-1] + 10;
-          } else if(!isFrameTen) {
-            additionalStrikeCounter[index-1] = additionalStrikeCounter[index-1] + bowlInteger;
-          }
+          additionalStrikeCounter[index-1] += whatToAddOneAgo(frame, bowl, bowlIndex, bowlInteger);
         }
         // if both two and one frames ago had a strike, add only the first number of the frame to it
         if((additionalStrikeCounter[index-2] !== undefined) && (additionalStrikeCounter[index-1] !== undefined) && (bowlIndex === 0)){
-          if(bowl === 'X') {
-            additionalStrikeCounter[index-2] = additionalStrikeCounter[index-2] + 10;
-          } else {
-            additionalStrikeCounter[index-2] = additionalStrikeCounter[index-2] + bowlInteger;
-          }
+          additionalStrikeCounter[index-2] += whatToAddTwoAgoOrSpare(bowl, bowlInteger);
         }
-
        if(additionalSpareCounter[index-1] !== undefined && (bowlIndex === 0)) {
-          if(bowl === 'X') {
-            additionalSpareCounter[index-1] = additionalSpareCounter[index-1] + 10;
-          } else {
-            additionalSpareCounter[index-1] = additionalSpareCounter[index-1] + bowlInteger;
-          }
+          additionalSpareCounter[index-1] += whatToAddTwoAgoOrSpare(bowl, bowlInteger);
         }
       } else {
+        // last frame
         if(bowl === 'X') {
           score += 10;
         } else if(frame.includes('/')) {
           if(bowlIndex === 1) {
-             score += 10;
-           } else if(bowlIndex === 2) {
-             score += bowlInteger;
-           }
+            score += 10;
+          } else if(bowlIndex === 2) {
+            score += bowlInteger;
+          }
         } else {
           score += bowlInteger;
         }
-        
         // if last bowl was a strike, indexes 0 and 1 should get added - strike is 10, spares get 10 on index 1
         if((additionalStrikeCounter[index-1] !== undefined) && bowlIndex<2) {
-          if(isFrameTen && ((bowl === 'X') || (bowlIndex === 1))) {
-            additionalStrikeCounter[index-1] = additionalStrikeCounter[index-1] + 10;
-          } else if(!isFrameTen) {
-            additionalStrikeCounter[index-1] = additionalStrikeCounter[index-1] + bowlInteger;
-          }
+          additionalStrikeCounter[index-1] += whatToAddOneAgo(frame, bowl, bowlIndex, bowlInteger);
         }
         // if two bowls ago was a strike, index 0 should get added - strike is 10
         if((additionalStrikeCounter[index-2] !== undefined) && (additionalStrikeCounter[index-1] !== undefined) && (bowlIndex === 0)){
-          if(bowl === 'X') {
-            additionalStrikeCounter[index-2] = additionalStrikeCounter[index-2] + 10;
-          } else {
-            additionalStrikeCounter[index-2] = additionalStrikeCounter[index-2] + bowlInteger;
-          }
+          additionalStrikeCounter[index-2] += whatToAddTwoAgoOrSpare(bowl, bowlInteger);
         }
-
         if((additionalSpareCounter[index-1] !== undefined) && bowlIndex === 0) {
-          if(bowl === 'X') {
-            additionalSpareCounter[index-1] = additionalSpareCounter[index-1] + 10;
-          } else {
-            additionalSpareCounter[index-1] = additionalSpareCounter[index-1] + bowlInteger;
-          }
+          additionalSpareCounter[index-1] += whatToAddTwoAgoOrSpare(bowl, bowlInteger);
         }
       }
     });
@@ -91,6 +64,21 @@ function bowling(frames) {
     score += additionalSpareCounter[key];
   }
   return score;
+}
+
+function whatToAddOneAgo(frame, bowl, bowlIndex, bowlInteger) {
+  const isFrameTen = (bowl === 'X' || frame.includes('/')) ? true : false;
+  if(isFrameTen && ((bowl === 'X') || (bowlIndex === 1))) {
+    return 10;
+  } else if(!isFrameTen) {
+    return bowlInteger;
+  } else {
+    return 0;
+  }
+}
+
+function whatToAddTwoAgoOrSpare(bowl, bowlInteger) {
+  return (bowl === 'X') ? 10 : bowlInteger;
 }
 
 module.exports = bowling;
